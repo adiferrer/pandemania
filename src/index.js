4bc2870756d8829f192f82ca22d397684dd48120
Adi <adi@macbook.lan>
2023-08-18 22:14:18 +0800

    Finish rough draft of table of contents page

--- src/index.js
+++ src/index.js
@@ -1,69 +1,121 @@
 import "./styles.css";
 import shufflePlaceholder from "./assets/images/shufflePlaceholder.jpeg";
 
+// helper functions
+
+function createElementWithClass(tagName, className, textContent) {
+  const element = document.createElement(tagName);
+  element.classList.add(className);
+  if (textContent) {
+    element.textContent = textContent;
+  }
+  return element;
+}
+
+function createContentDiv(number, title, author) {
+  const contentDiv = document.createElement("div");
+
+  const contentNumber = createElementWithClass("h2", 'TOCNumber', number);
+  contentNumber.textContent = number;
+
+  const contentRight = document.createElement("div");
+
+  const contentTitle = createElementWithClass("h2", 'TOCTitle', title);
+  contentTitle.textContent = title;
+
+  const contentCredit = createElementWithClass("h3", 'TOCCredit', author);
+  contentCredit.textContent = `BY: ${author}`;
+
+  contentRight.appendChild(contentTitle);
+  contentRight.appendChild(contentCredit);
+
+  contentDiv.appendChild(contentNumber);
+  contentDiv.appendChild(contentRight);
+
+  return contentDiv;
+}
+
+// render functions
+
 function renderCoverPage() {
-  const cover = document.createElement("div");
-  cover.classList.add("cover");
-  const coverTitle = document.createElement("h1");
-  coverTitle.textContent = "Pandemania";
-//   const coverImage = new Image();
-//   coverImage.src = shufflePlaceholder;
+  const cover = createElementWithClass("div", "cover");
+  const coverTitle = createElementWithClass("h1", null, "Pandemania");
+  const coverImage = new Image();
+  coverImage.src = shufflePlaceholder;
   cover.appendChild(coverTitle);
-//   cover.appendChild(coverImage);
+  cover.appendChild(coverImage);
   return cover;
 }
 
 function renderTableOfContents() {
   const tableOfContents = document.createElement("div");
   tableOfContents.classList.add("tableOfContents");
+  
   const leftTOC = document.createElement("div"); // left side of table of contents
   leftTOC.classList.add("leftTOC");
   const verticalText = document.createElement("div");
   verticalText.textContent = "Life in the Pandemic";
   leftTOC.appendChild(verticalText);
+
   const rightTOC = document.createElement("div"); // right side of table of contents
   rightTOC.classList.add("rightTOC");
+
   const titleDivTOC = document.createElement("div");
+  titleDivTOC.classList.add("titleDivTOC");
   const titleTopTOC = document.createElement("h2");
   titleTopTOC.textContent = "Anxiety";
   const titleBottomTOC = document.createElement("h2");
   titleBottomTOC.textContent = "Aspirations";
   const titleSubTOC = document.createElement("h3");
   titleSubTOC.textContent = "Contents";
+
+  const contentsTOC = document.createElement("div");
+  contentsTOC.classList.add("contentsTOC");
+  const contentData = [
+    { number: "01", title: "The Mask", author: "Bulabos, Kyla Cresly" },
+    { number: "02", title: "The Formula", author: "Portillo, Kyle Andre" },
+    { number: "03", title: "a moment in my head", author: "Rosario, Mara Dominique" },
+    { number: "04", title: "A New Challenge", author: "Santos, Aleco Emmanuel" },
+    { number: "05", title: "Caged", author: "Ventura, Diane Grace" },
+    { number: "06", title: "FUTURE", author: " Uy, Corven Alfred Joseph" },
+    { number: "07", title: "The Overman", author: "Dinglasan, Chris Emmanuel" },
+  ];
+  contentData.forEach(data => {
+    const contentDiv = createContentDiv(data.number, data.title, data.author);
+    contentDiv.classList.add("contentDivTOC");
+    contentsTOC.appendChild(contentDiv);
+  });
+
   titleDivTOC.appendChild(titleTopTOC);
   titleDivTOC.appendChild(titleBottomTOC);
   titleDivTOC.appendChild(titleSubTOC);
   rightTOC.appendChild(titleDivTOC);
+  rightTOC.appendChild(contentsTOC);
   tableOfContents.appendChild(leftTOC);
   tableOfContents.appendChild(rightTOC);
   return tableOfContents;
 }
 
-// const theMask = document.createElement("div");
-// const theFormula = document.createElement("div");
-// const aMomentInMyHead = document.createElement("div");
-// const aNewChallenge = document.createElement("div");
-// const caged = document.createElement("div");
-// const future = document.createElement("div");
-// const theOverman = document.createElement("div");
-
-function renderFooter(footerContent) {
-  const footer = document.createElement("footer");
-  const footerText = document.createElement("p");
-  footerText.innerHTML = footerContent;
+function renderFooter() {
+  const footer = createElementWithClass("footer", null);
+  const footerText = createElementWithClass(
+    "p",
+    null,
+    `© ${new Date().getFullYear()} Pandemania`
+  );
   footer.appendChild(footerText);
   return footer;
 }
 
-function renderContainer(arrOfDivs) {
-  const container = document.createElement("div");
-  container.classList.add("container");
-  arrOfDivs.forEach((div) => {
-    container.appendChild(div);
-  });
-  return container;
+function renderPage() {
+  const container = createElementWithClass("div", "container");
+  const components = [
+    renderCoverPage(),
+    renderTableOfContents(),
+    renderFooter()
+  ];
+  container.append(...components);
+  document.body.appendChild(container);
 }
 
-document.body.appendChild(
-  renderContainer([renderCoverPage(), renderTableOfContents(), renderFooter('© 2021 Pandemania')])
-);
+renderPage();
